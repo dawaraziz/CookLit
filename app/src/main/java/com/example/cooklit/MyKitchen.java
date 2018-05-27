3cd08a31db57e451bd742f8be0d3ea6fc82f1dea
Sean Kim <sh32kim@uwaterloo.ca>
2018-05-27 18:37:34 -0400

    merged sean_qian2

--- app/src/main/java/com/example/cooklit/MyKitchen.java
+++ app/src/main/java/com/example/cooklit/MyKitchen.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -59,20 +60,10 @@ public class MyKitchen extends AppCompatActivity {
 
         });
 
-<<<<<<< HEAD
+
         // Qian Editing (TOP)
         Button cooklit_button = (Button) findViewById(R.id.cooklit_button);
-=======
-        Button cooklit_button = findViewById(R.id.cooklit_button);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
->>>>>>> 794664828f426c1d94613653765c61151f9c142b
+
 
         cooklit_button.setOnClickListener(new View.OnClickListener() {
             @Override
